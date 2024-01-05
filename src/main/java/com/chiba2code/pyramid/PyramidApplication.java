532f16253d1083c32601ba11a45f68fe1b124fd4
chingunjav battsogt <chingunjav@gmail.com>
2024-01-04 21:12:38 -0600

    first commit

---
+++ src/main/java/com/chiba2code/pyramid/PyramidApplication.java
@@ -0,0 +1,13 @@
+package com.chiba2code.pyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyramidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiba2code/pyramid/PyramidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiba2code.pyramid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyramidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
